c5128e916cb4277c790560b713209a33c0bcd3bc
Adrian Delgado <adriandelgadoc1@gmail.com>
2022-02-06 23:22:44 -0800

    Fixed up button group

--- pages/queens.tsx
+++ pages/queens.tsx
@@ -13,32 +13,43 @@ const Queens: NextPage = () => {
 	return (
 		<section>
 			<h1 className="main-heading">Queens</h1>
-			<div className="btn-group justify-center mb-12">
+
+			<div className="flex justify-center rounded-md shadow-sm" role="group">
 				<button
+					type="button"
 					onClick={() => setCurrentPage('british')}
-					className={`btn btn-outline ${
-						currentPage === 'british' && 'btn-active'
-					}`}
+					className={`py-3 px-5 text-sm font-medium text-gray-900
+					bg-white rounded-l-lg border border-gray-200
+					hover:bg-gray-100 hover:text-blue-700 focus:z-10
+					focus:ring-2 focus:ring-blue-700 focus:text-blue-700
+					${currentPage === 'british' && 'btn-active'}`}
 				>
 					British Shorthair
 				</button>
 				<button
+					type="button"
 					onClick={() => setCurrentPage('ragdoll')}
-					className={`btn btn-outline ${
-						currentPage === 'ragdoll' && 'btn-active'
-					}`}
+					className={`py-3 px-5 text-sm font-medium text-gray-900
+					bg-white border-t border-b border-gray-200 hover:bg-gray-100
+					hover:text-blue-700 focus:z-10 focus:ring-2
+					focus:ring-blue-700 focus:text-blue-700
+					${currentPage === 'ragdoll' && 'btn-active'}`}
 				>
 					Ragdoll
 				</button>
 				<button
+					type="button"
 					onClick={() => setCurrentPage('scottish')}
-					className={`btn btn-outline ${
-						currentPage === 'scottish' && 'btn-active'
-					}`}
+					className={`py-3 px-5 text-sm font-medium text-gray-900 bg-white
+					rounded-r-md border border-gray-200 hover:bg-gray-100
+					hover:text-blue-700 focus:z-10 focus:ring-2
+					focus:ring-blue-700 focus:text-blue-700
+					${currentPage === 'scottish' && 'btn-active'}`}
 				>
 					Scottish Fold
 				</button>
 			</div>
+
 			{currentPage === 'british' ? (
 				<QueenCat catList={britishQueenList} />
 			) : null}
