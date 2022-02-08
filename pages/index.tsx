2251f8b3a5f5cfedd3223cde9547922fba1b4f20
Adrian Delgado <adriandelgadoc1@gmail.com>
2022-02-08 10:20:15 -0800

    Touch ups

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,15 +11,24 @@ const Home: NextPage = () => {
 			</Head>
 			<section
 				className="container mx-auto flex px-5 md:pb-24
-				md:pt-16 md:flex-row flex-col-reverse
+				md:pt-16 md:flex-row flex-col
 				justify-center items-center pt-10"
 			>
-				<div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+				<img
+					className="w-1/2 mb-8 md:hidden"
+					src="/fulllogo.webp"
+					alt="LuxuryOnly Logo"
+				/>
+				<div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0">
 					<MainCarousel />
 				</div>
-				<div className="md:w-1/2 lg:pl-28 md:pl-16 flex flex-col md:items-start items-center">
-					<img className="xl:ml-8" src="/fulllogo.webp" alt="LuxuryOnly Logo" />
-					<p className="my-8 max-w-lg leading-relaxed md:text-left text-center">
+				<div className="md:w-1/2 lg:pl-28 md:pl-16 flex flex-col  items-center">
+					<img
+						className="xl:ml-8 w-2/3 hidden md:inline"
+						src="/fulllogo.webp"
+						alt="LuxuryOnly Logo"
+					/>
+					<p className="my-8 max-w-lg px-5 leading-relaxed">
 						We treat our kittens as our children who are raised with love, care,
 						and dedication. We are proud of our distinguished, prize winning
 						kittens and only offer them into good homes where they will be loved
@@ -34,18 +43,16 @@ const Home: NextPage = () => {
 
 			<section className="mb-28 mt-10">
 				<h2 className="text-center text-2xl">Our Specialty Breeds</h2>
-				<div className="flex flex-col basis-full justify-center items-start md:flex-row">
+				<div className="flex flex-col basis-full justify-center md:items-stretch items-center md:flex-wrap md:flex-row">
 					<div className="nft-card">
 						<img
-							className="rounded-t-lg"
+							className="rounded-t-lg object-cover w-full md:h-72 h-full"
 							src="/Kittens/Landscape/kitten11.webp"
 							alt=""
 						/>
 						<div className="p-5">
-							<h5 className="mb-2 text-2xl text-center font-bold tracking-tight text-gray-900">
-								British Shorthairs
-							</h5>
-							<p className="mb-3 font-normal text-gray-700">
+							<h5 className="breed-card-heading">British Shorthairs</h5>
+							<p className="p-4 text-gray-700">
 								British Shorthair cats are an absolute joy to own. These
 								affectionate pets are ideal companions for families with
 								children and other pets due to their easy going nature, but they
@@ -59,15 +66,14 @@ const Home: NextPage = () => {
 					</div>
 					<div className="nft-card">
 						<img
-							className="rounded-t-lg"
+							className="rounded-t-lg object-cover w-full md:h-72 h-full"
 							src="/Kittens/Landscape/kitten10.webp"
 							alt=""
 						/>
+
 						<div className="p-5">
-							<h5 className="mb-2 text-2xl text-center font-bold tracking-tight text-gray-900">
-								Ragdolls
-							</h5>
-							<p className="mb-3 font-normal text-gray-700">
+							<h5 className="breed-card-heading">Ragdolls</h5>
+							<p className="p-4 text-gray-700">
 								Ragdoll cats are social, happily spend time in the house of the
 								owner and rarely wander out on their own. They love being with
 								the owner, they yearn for attention and enjoy the company of
@@ -82,15 +88,14 @@ const Home: NextPage = () => {
 					</div>
 					<div className="nft-card">
 						<img
-							className="rounded-t-lg"
+							className="rounded-t-lg object-cover w-full md:h-72 h-full"
 							src="/Kittens/Landscape/kitten9.webp"
 							alt=""
 						/>
+
 						<div className="p-5">
-							<h5 className="mb-2 text-2xl text-center font-bold tracking-tight text-gray-900 ">
-								Scottish Folds
-							</h5>
-							<p className="mb-3 font-normal text-gray-700">
+							<h5 className="breed-card-heading">Scottish Folds</h5>
+							<p className="p-4 font-normal text-gray-700">
 								The Scottish Fold is a sweet, charming breed. They are an easy
 								cat to live with and to care for. They are affectionate and are
 								comfortable with all members of their family. They are attentive
--- src/components/Home/MainCarousel.tsx
+++ src/components/Home/MainCarousel.tsx
@@ -4,7 +4,7 @@ import { photosForMain } from './homeCarouselPhotos';
 export function MainCarousel() {
 	return (
 		<Carousel
-			className="max-w-xl overflow-hidden text-center mx-auto"
+			className="max-w-xl overflow-hidden text-center mx-auto rounded-2xl"
 			autoPlay={true}
 			infiniteLoop={true}
 			interval={4000}
