54aeff13db5e873ebaedad329b3371381c155fcc
luxuryonly <99926300+luxuryonly@users.noreply.github.com>
2024-01-29 00:30:54 +0100

    remove pic

--- pages/about.tsx
+++ pages/about.tsx
@@ -22,20 +22,7 @@ const About: NextPage = () => {
 					</div>
 					<div className="flex flex-col sm:flex-row mt-10">
 						<div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-							<!-- <div className="w-40 h-40 inline-flex items-center justify-center text-gray-400">
-								<img className="rounded-full" src="/elena.webp" alt="" />
-							</div>
-							<div className="flex flex-col items-center text-center justify-center">
-								<h2 className="font-medium title-font mt-4 text-gray-900 text-lg">
-									Elena Maximova
-								</h2>
-								<div className="w-12 h-1 bg-beige-800 rounded mt-2 mb-4"></div>
-								<p className="text-base">
-									Owner of Luxury Only.
-									<br />
-									Mother of many beautiful cats.
-								</p>
-							</div> -->
+						
 						</div>
 						<div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0">
 							<p className="leading-relaxed text-lg mb-4">
